94a9f23ef13b06ddf81a7f29d4afa6e31c4ebd3f
Ryosuke Kamesawa <emak.ryo@gmail.com>
2018-07-01 12:11:17 +0900

    AC ARC58/D

--- src/arc58/d.cpp
+++ src/arc58/d.cpp
@@ -1,34 +1,79 @@
 #include<iostream>
+#include<iomanip>
+//#include<cstdio>
+#include<vector>
+#include<map>
+#include<queue>
 #include<algorithm>
+#include<cmath>
+#include<cassert>
 using namespace std;
-const int HWmax = 1000000;
-const long P = 1000000007;
-int H,W,A,B;
+typedef long long ll;
+const ll P = 1e9+7;
+const int COMB_MAX = 300000;
+ll fact[COMB_MAX];
 
-long ext_gcd(long a, long b, long &x, long &y){
+ll ext_gcd(ll a, ll b, ll &x, ll &y){
 	if(b == 0){
 		x = 1;
 		y = 0;
 		return a;
 	}
-	else{
-		long gcd = ext_gcd(b, a%b, x, y);
-		return gcd;
+	ll nx, ny;
+	ll g = ext_gcd(b, a%b, nx, ny);
+	x = ny;
+	y = nx - a/b *ny;
+	return g;
+}
+
+ll inv_mod(ll x, ll p){
+	ll y,z;
+	ext_gcd(x, p, y, z);
+	return y>=0 ? y : p+(y%p);
+}
+
+ll fact_mod(ll n, ll p, ll &e){
+	// n! = a * p^e
+	// return = a % p
+	if(n == 0){
+		e = 0;
+		return 1;
 	}
+
+	ll na = fact_mod(n/p, p, e);
+
+	e += n/p;
+	ll a = na * fact[n%p] % p;
+
+	if((n/p)%2) a = a * (p-1)%p;
+	return a;
+}
+
+ll comb_mod(ll n, ll m, ll p){
+	ll e1,e2,e3;
+	ll a1 = fact_mod(n, p, e1);
+	ll a2 = fact_mod(m, p, e2);
+	ll a3 = fact_mod(n-m, p, e3);
+
+	if(e1 > e2+e3) return 0;
+	else return a1*inv_mod(a2*a3%p, p)%p;
 }
 
 int main(){
+	fact[0] = fact[1] = 1;
+	for(int i=2; i<COMB_MAX; i++) fact[i] = fact[i-1]*i%P;
+
+	int H, W, A, B;
 	cin >> H >> W >> A >> B;
 
-	for(int i=0; i<H+W; i++){
-		if(i < H-A){
-		}
-		else if(i < H-A < H+B){
-		}
-		else{
-		}
+	ll all = comb_mod(W+H-2, W-1, P);
+	for(int i=0; i<B; i++){
+		ll x = comb_mod(i+H-A-1, i, P) * comb_mod(W-i-1+A-1, A-1, P) % P;
+		all = (all + P - x) % P;
 	}
 
-	cout << endl;
+	cout << all << endl;
+
+
 	return 0;
 }
