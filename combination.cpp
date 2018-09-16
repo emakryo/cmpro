390eda3ae12eddca7cb4bffd48d147f6efae1290
Ryosuke Kamesawa <ryosuke.kamesawa@dena.com>
2018-09-16 14:47:21 +0900

    Add documents to library functions

--- combination.cpp
+++ combination.cpp
@@ -3,6 +3,9 @@ using namespace std;
 typedef long long ll;
 ll fact[100];
 
+// Extended Euclid's greatest common divisor algorithm
+// Find (x, y, g)
+// where $a*x + b*y = g$ and g is the greatest common divisor of (a, b)
 ll ext_gcd(ll a, ll b, ll &x, ll &y){
 	if(b == 0){
 		x = 1;
@@ -16,15 +19,33 @@ ll ext_gcd(ll a, ll b, ll &x, ll &y){
 	return g;
 }
 
+// Modular inverse of x given modulo p, y
+// x*y = 1 mod p
+// <=> x*y + z*p = 1
 ll inv_mod(ll x, ll p){
 	ll y,z;
 	ext_gcd(x, p, y, z);
 	return y>=0 ? y : p+(y%p);
 }
 
+
+// Modular factorial of n, also counting p's appearance e
+// n! = a * p^e
+// return: a % p
+// n! = n * (n-1) * ... * (k*p+1) *
+//      (k*p) * (k*p-1) * ... ((k-1)*p+1) *
+//      ...
+//      p * (p-1) * ... * 2 * 1   (k = floor(n/p))
+//    = (n%p) * ... * 1 *
+//      (k*p) * (p-1) * ... * 1 *
+//      p * (p-1) * ... * 1       (mod p, preserving p's multiples)
+//    = (n%p)! * p^k * k! * (p-1)^k
+//    = (n%p)! * p^k * k! * (p-1)^(k%2)
+//
+// From the facts:
+//   (p-1)! = p-1 (mod p) (c.f. Willson's theorem)
+//   (p-2)**2 = 1 (mod p)
 ll fact_mod(ll n, ll p, ll &e){
-	// n! = a * p^e
-	// return = a % p
 	if(n == 0){
 		e = 0;
 		return 1;
@@ -39,6 +60,11 @@ ll fact_mod(ll n, ll p, ll &e){
 	return a;
 }
 
+
+// Modular combination (n, m) given module p
+// (n, m) = n!/(n-m)!m!
+//        = (a_n * p^(e_n)) / ((a_{n-m} * p^(e_{n-m}) * (a_m * p^e_m))
+//        = a_n / (a_{n-m} * a_m) * p^(e_n - e_{n-m} - e_m)
 ll comb_mod(ll n, ll m, ll p){
 	ll e1,e2,e3;
 	ll a1 = fact_mod(n, p, e1);
--- union_find.cpp
+++ union_find.cpp
@@ -3,11 +3,12 @@
 using namespace std;
 
 class UnionFind{
-	vector<int> par, rnk;
+	vector<int> par, rnk, cnt;
 	public:
 	UnionFind(int n){
 		par=vector<int>(n);
 		rnk=vector<int>(n,0);
+		cnt=vector<int>(n,1);
 		for(int i=0; i<n; i++) par[i]=i;
 	}
 	int root(int i){
@@ -20,17 +21,23 @@ class UnionFind{
 		if(ri==rj) return;
 
 		if(rnk[ri]==rnk[rj]){
+			cnt[rj] += cnt[ri];
 			par[ri] = rj;
 			rnk[rj]++;
 		}
 		else if(rnk[ri] < rnk[rj]){
+			cnt[rj] += cnt[ri];
 			par[ri] = rj;
 		}
 		else{
+			cnt[ri] += cnt[rj];
 			par[rj] = ri;
 		}
 	}
 	bool same(int i, int j){
 		return root(i) == root(j);
+	}
+	int count(int i){
+		return cnt[root(i)];
 	}
 };
