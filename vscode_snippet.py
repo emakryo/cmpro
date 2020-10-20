06810e708bd40be831588bf1399a3f4ce0da922f
Ryosuke Kamesawa <emak.ryo@gmail.com>
2020-10-21 00:58:05 +0900

    Add snippets

--- vscode_snippet.py
+++ vscode_snippet.py
@@ -2,13 +2,14 @@ import json
 from pathlib import Path
 
 
-def create_snippets(source_dir: Path, out_file: Path, extension: str):
+def create_snippets(source_dir: Path, out_file: Path, extension: str, scope: str):
     out = {}
     for cpp in source_dir.glob(f"*.{extension}"):
         lines = list(l.rstrip() for l in open(cpp))
         name = cpp.name
         out[name] = {
             "prefix": name,
+            "scope": scope,
             "body": lines,
             "description": str(cpp),
         }
@@ -18,7 +19,4 @@ def create_snippets(source_dir: Path, out_file: Path, extension: str):
 
 
 if __name__ == "__main__":
-    output = Path("./snippets")
-    output.mkdir(parents=True, exist_ok=True)
-
-    create_snippets(Path("./cpp"), output / "cpp.json", "cpp")
+    create_snippets(Path("./cpp"), Path(".vscode/cpp.code-snippets"), "cpp", "cpp")
