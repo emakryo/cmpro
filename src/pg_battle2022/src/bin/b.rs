6244be6cf70d77335dbf9bfc1225a7934d1dfaed
Ryosuke Kamesawa <emak.ryo@gmail.com>
2022-12-27 13:19:34 +0900

    update

--- src/pg_battle2022/src/bin/b.rs
+++ src/pg_battle2022/src/bin/b.rs
@@ -23,18 +23,30 @@ fn main() {
     let sqs: BTreeMap<i64, i64> = (0..1000000).map(|i| (i*i, i)).collect();
 
     let mut ans = 0;
-    for p in -t..=t {
-        let m2 = t * t - (w * p + g.0 - s.0).pow(2);
+    ans += count(g.0 - s.0, g.1 - s.1, w, h, t, &sqs);
+    ans += count(g.0 - s.0, 2 * h - g.1 - s.1, w, h, t, &sqs);
+    ans += count(2 * w - g.0 - s.0, g.1 - s.1, w, h, t, &sqs);
+    ans += count(2 * w - g.0 - s.0, 2 * h - g.1 - s.1, w, h, t, &sqs);
+
+    println!("{}", ans);
+}
+
+
+fn count(a: i64, b: i64, w: i64, h: i64, t: i64, sqs: &BTreeMap<i64, i64>) -> i32 {
+    let mut ret = 0;
+
+    for x in -t..=t {
+        let m2 = t * t - (2 * w * x - a).pow(2);
         if let Some(&m) = sqs.get(&m2) {
-            if (m - (g.1 - s.1)).abs() % h == 0 {
-                ans += 1;
+            if (m - b).abs() % (2 * h) == 0 {
+                ret += 1;
             }
 
-            if m > 0 && (- m - (g.1 - s.1)).abs() % h == 0 {
-                ans += 1;
+            if m > 0 && (-m - b).abs() % (2 * h) == 0 {
+                ret += 1;
             }
         }
     }
 
-    println!("{}", ans);
+    ret
 }
