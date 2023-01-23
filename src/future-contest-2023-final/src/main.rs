81db478cc7da06a9b99a7888e39952ddb82cdbe5
Ryosuke Kamesawa <emak.ryo@gmail.com>
2023-01-23 21:07:03 +0900

    update

--- src/future-contest-2023-final/src/main.rs
+++ src/future-contest-2023-final/src/main.rs
@@ -2,6 +2,8 @@
 use std::f64::consts::PI;
 const TAU: f64 = 2.0 * PI;
 
+const R: i64 = 1_000_000_000;
+
 use std::io::{Read, BufRead};
 macro_rules! dbg {
     ($($xs:expr),+) => {
@@ -113,11 +115,15 @@ struct Query {
 }
 
 impl Query {
+    fn new(x: i64, y: i64) -> Self {
+        assert!(x*x + y*y <= R.pow(2), "x={}, y={}, norm2={}", x, y, x*x + y*y);
+        Self { x, y }
+    }
     fn random() -> Self {
-        let r: f64 = rand::random::<f64>() * 1000_000_000.0;
+        let r: f64 = rand::random::<f64>() * R as f64;
         let t: f64 = rand::random::<f64>() * 2.0 * PI;
 
-        Self { x: (r * t.cos()) as i64, y: (r * t.sin()) as i64 }
+        Self::new((r * t.cos()) as i64, (r * t.sin()) as i64)
     }
     fn midpoint(&self, other: &Self) -> Self {
         Self {
@@ -131,9 +137,22 @@ impl Query {
     }
 
     fn mov(&self, dir: f64, dist: f64) -> Self {
-        Self {
-            x: self.x + (dist * dir.cos()) as i64,
-            y: self.y + (dist * dir.sin()) as i64,
+        let x = self.x + (dist * dir.cos()) as i64;
+        let y = self.y + (dist * dir.sin()) as i64;
+        if x*x + y*y <= R.pow(2) {
+            Self::new(x, y)
+        } else {
+            let x0 = self.x as f64;
+            let y0 = self.y as f64;
+            let b = x0 * dir.cos() + y0 * dir.sin();
+            let c = x0 * x0 + y0 * y0 - (R * R) as f64;
+            let (_, k) = solve_quad2(1.0, b, c);
+            let k = k - 10.0;
+    
+            let x1 = x0 + k * dir.cos();
+            let y1 = y0 + k * dir.sin();
+    
+            Self::new(x1 as i64, y1 as i64)
         }
     }
 }
@@ -142,13 +161,20 @@ struct State {
     sigma: f64,
     sigma2: f64,
     turn: i64,
-    radius: i64,
     hist: Vec<(Query, Response)>,
     not_found: Vec<(Query, Response, f64)>,
     between: [Option<(Query, Response)>; 2],
     next: Query,
     err: bool,
-    targets: Vec<(i64, i64)>,
+    found_targets: Vec<(i64, i64)>,
+}
+
+fn o(msg: &str) {
+    println!("# {}", msg);
+}
+
+fn c(x: i64, y: i64, r: u32, g: u32, b: u32) {
+    println!("#c {} {} {} {} {}", x, y, r, g, b)
 }
 
 impl State {
@@ -157,25 +183,18 @@ impl State {
             sigma,
             sigma2: sigma.powi(2),
             turn: 0,
-            radius: 1000000000,
             hist: vec![],
             not_found: vec![],
             between: [None, None],
             next: Query::random(),
             err: false,
-            targets: vec![],
-        }
-    }
-
-    fn d(&self, msg: &str) {
-        if self.turn < 0 {
-            eprintln!("{}", msg);
+            found_targets: vec![],
         }
     }
 
     fn error(&mut self) -> Query {
         self.err = true;
-        Query { x: 0, y: 0 }
+        Query::new(0, 0)
     }
 
     fn next_query(&mut self) -> Query {
@@ -188,19 +207,25 @@ impl State {
 
     fn push(&mut self, r: Response) {
         self.turn += 1;
-        self.d(&format!("turn={}", self.turn));
+        o(&format!("turn={}", self.turn));
         let q = self.next;
         self.hist.push((q, r));
 
         if let Response::Hit(x, y) = r {
-            self.targets.push((x, y));
+            self.found_targets.push((x, y));
 
-            self.next = self.remove_found();
-            self.d(&format!("next={:?}", &self.next));
+            self.remove_found();
+
+            self.next = if let Some(n) = self.find_between() {
+                n
+            } else {
+                self.find_unbiguous()
+            };
+            o(&format!("next={:?}", &self.next));
             return;
         }
 
-        self.not_found.push((q, r, 1e10));
+        self.push_not_found(q, r);
 
         if self.not_found.len() < 10000 {
             self.next = self.next_from_between(q, r);
@@ -210,13 +235,12 @@ impl State {
 
     }
 
-    fn between_edge(&self) -> Query {
-        let q = self.between[0].unwrap().0;
-        let t0 = self.between[0].unwrap().1.dir();
+    fn between_edge(&self, q: Query, r: Response) -> (Query, f64) {
+        let t0 = r.dir();
 
         let x0 = q.x as f64;
         let y0 = q.y as f64;
-        let rad = self.radius as f64;
+        let rad = R as f64;
         let b = x0 * t0.cos() + y0 * t0.sin();
         let c = x0 * x0 + y0 * y0 - rad * rad;
         let (_, k) = solve_quad2(1.0, b, c);
@@ -226,13 +250,13 @@ impl State {
         let y1 = y0 + k * t0.sin();
 
         assert!((x1*x1 + y1*y1 - rad*rad).abs() / (rad * rad) < 1e-4);
-        Query {x: x1 as i64, y: y1 as i64}.midpoint(&q)
+        (Query::new(x1 as i64, y1 as i64).midpoint(&q), k)
     }
 
-    fn remove_found(&mut self) -> Query {
+    fn remove_found(&mut self) {
         let mut not_found = vec![];
         let dist_threshold = 3.0;
-        let target = self.targets.last().unwrap();
+        let target = self.found_targets.last().unwrap();
 
         for &(q, r, min_dist) in &self.not_found {
             let dist = min_dist.min(self.normalized_dist(q, r, *target));
@@ -242,15 +266,70 @@ impl State {
             }
         }
 
-        self.d(&format!("not_found.len()={:?}", not_found.len()));
+        o(&format!("not_found.len()={:?}", not_found.len()));
         self.not_found = not_found;
 
+    }
+
+    fn find_between(&mut self) -> Option<Query> {
+        let mut between = None;
+        let mut ret = None;
+        let mut score = 0;
+        let thresh = 3.0 * self.sigma;
+        for i in 0..self.not_found.len() {
+            for j in i+1..self.not_found.len() {
+                let r0 = self.not_found[i].1;
+                let r1 = self.not_found[j].1;
+                if r0.opposite_dir(&r1) {
+                    let q0 = self.not_found[i].0;
+                    let q1 = self.not_found[j].0;
+                    let t0 = r0.dir();
+                    let t1 = r1.dir();
+                    let t2 = ((q0.y - q1.y) as f64).atan2((q0.x - q1.x) as f64);
+
+                    if (t1 - t2).abs().min((t1 - t2 + TAU).abs()).min((t1 - t2 - TAU).abs()) < thresh {
+                        let s = q0.dist2(&q1);
+                        if s < score {
+                            between = Some([Some((q0, r0)), Some((q1, r1))]);
+                            ret = Some(q0.midpoint(&q1));
+                            score = s;
+                        }
+                    }
+                }
+            }
+        }
+
+        if ret.is_some() {
+            o("find_between");
+        }
+
+        ret
+    }
+
+    fn find_unbiguous(&mut self) -> Query {
         if self.not_found.is_empty() {
+            self.between = [None, None];
             Query::random()
         } else {
-            let farthest = self.not_found.iter().max_by_key(|(_, _, l)| *l as i64).unwrap();
-            self.between = [Some((farthest.0, farthest.1)), None];
-            self.between_edge()
+            // if let Some(&(q, r, _)) = self.not_found.iter().filter(|(_, _, d)| *d > PI / 4.0).min_by_key(|(q, r, _)| {
+            //     let k = self.between_edge(*q, *r).1 as i64;
+            //     o(&format!("k={}", k));
+            //     k
+            // }) {
+            //     self.between = [Some((q, r)), None];
+            //     return self.between_edge(q, r).0
+            // }
+
+            // let mut farthest = self.not_found.iter_mut().max_by_key(|(_, _, l)| *l as i64).unwrap();
+            // self.between = [Some((farthest.0, farthest.1)), None];
+            // farthest.2 /= 2.0;
+            // let (q, r, _) = *farthest;
+            // self.between_edge(q, r).0
+
+            let idx = rand::random::<usize>() % self.not_found.len();
+            let (q, r, _) = self.not_found[idx];
+            self.between = [Some((q, r)), None];
+            self.between_edge(q, r).0
         }
     }
 
@@ -277,11 +356,11 @@ impl State {
         match self.between {
             [None, None] => {
                 self.between[0] = Some((q, r));
-                self.between_edge()
+                self.between_edge(q, r).0
             }
             [Some((_, r0)), None] if r0.same_dir(&r) => {
                 self.between[0] = Some((q, r));
-                self.between_edge()
+                self.between_edge(q, r).0
             }
             [Some((q0, r0)), None] if r0.opposite_dir(&r) => {
                 self.between[1] = Some((q, r));
@@ -292,7 +371,7 @@ impl State {
             }
             [Some(_), None] => {
                 self.between[0] = Some((q, r));
-                self.between_edge()
+                self.between_edge(q, r).0
             }
             [Some((q0, r0)), Some((q1, r1))]
             if r0.same_dir(&r) && midpoint_thresh < q1.dist2(&q) => {
@@ -328,6 +407,17 @@ impl State {
         }
 
         todo!()
+    }
+
+    fn push_not_found(&mut self, q: Query, r: Response) {
+        let dist_threshold = 3.0;
+        let mut min_dist = std::f64::MAX;
+        for &target in &self.found_targets {
+            min_dist = min_dist.min(self.normalized_dist(q, r, target));
+        }
+        if dist_threshold < min_dist {
+            self.not_found.push((q, r, min_dist));
+        }
     }
 
 }
@@ -375,7 +465,7 @@ fn disjoint(q0: Query, r0: Response, q1: Query, r1: Response) -> Option<Query> {
         let x = x0 + t0.cos() * k;
         let y = y0 + t0.sin() * k;
 
-        Some(Query { x: x as i64, y: y as i64})
+        Some(Query::new(x as i64, y as i64))
     } else {
         None
     }
--- src/future-contest-2023-final/test.sh
+++ src/future-contest-2023-final/test.sh
@@ -3,8 +3,10 @@ set -e
 
 cd tools
 
-for intxt in $(ls in/); do
-cargo run --release --bin tester bash -c "cd ..; cargo run" < in/$intxt > out.txt
-done
-# pbcopy < out.txt
+# export RUST_BACKTRACE=1
+# for intxt in $(ls in/); do
+# cargo run --release --bin tester bash -c "cd ..; cargo run" < in/$intxt > out.txt
+# done
+cargo run --release --bin tester bash -c "cd ..; cargo run" < in/0000.txt > out.txt
+pbcopy < out.txt
 cd -
